d77e1ddf7c165d189f4ea7fec9cdbd08058965f6
norasmiza <67034652+norasmiza@users.noreply.github.com>
2020-06-17 14:40:07 +0800

    update code

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-echo "This is a simple php app - edited"
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Random Number</title>
+    </head>
+    <body>
+        <p>Generating a random number between 1 and 10:
+            <?php
+
+            echo rand(1, 10);
+
+            ?>
+        </p>
+    </body>
+</html>
